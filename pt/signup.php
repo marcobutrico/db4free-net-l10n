b82215ebf4735513406b1a15ffd5e3a47876b6cc
marcobutrico <30322144+marcobutrico@users.noreply.github.com>
2017-07-20 13:02:25 -0300

    Update signup.php
    Texts adequations and spelling accent

--- pt/signup.php
+++ pt/signup.php
@@ -19,19 +19,19 @@ $translations = array(
 
     'agree-conditions' => 'Eu li e concordo com os <a href="conditions.php">termos de uso</a>.',
 
-    'ph1' => '4-16 caractéres., sem maiúsculas, 1º deve ser caractér.',
+    'ph1' => '4-16 caracteres., sem maiúsculas, 1º deve ser caracter.',
 
-    'ph2' => 'Min. 6 caractéres.',
+    'ph2' => 'Min. 6 caracteres.',
 
     'ph3' => 'Digite seu email',
 
     'explanation-5.7' => 'Database user and database name may contain lower case letters, numbers and the underscore and must be between 4 and 16 characters long. You must not use <a href="https://dev.mysql.com/doc/refman/5.7/en/reserved-words.html">reserved words</a>!',
 
-    'explanation-5.7' => 'O nome de usuario MySQL e o nome do banco de dados podem contar letras minusculas, numeros e "underline"(sublinhado) e deve ter entre 4 e 16 caractéres. Você não pode utilizar <a href="https://dev.mysql.com/doc/refman/5.7/en/reserved-words.html">palavras reservadas(link em inglês)</a>!',
+    'explanation-5.7' => 'O nome de usuário MySQL e o nome do banco de dados podem contar letras minusculas, numeros e "underline"(sublinhado) e deve ter entre 4 e 16 caracteres. Você não pode utilizar <a href="https://dev.mysql.com/doc/refman/5.7/en/reserved-words.html"> as palavras reservadas (link em inglês)</a>!',
 
     'explanation-8.0' => 'Database user and database name may contain lower case letters, numbers and the underscore and must be between 4 and 16 characters long. You must not use <a href="https://dev.mysql.com/doc/refman/8.0/en/reserved-words.html">reserved words</a>!',
 
-    'explanation-8.0' => 'O nome de usuario MySQL e o nome do banco de dados podem contar letras minusculas, numeros e "underline"(sublinhado) e deve ter entre 4 e 16 caractéres. Você não pode utilizar <a href="https://dev.mysql.com/doc/refman/8.0/en/reserved-words.html">palavras reservadas(link em inglês)</a>!',
+    'explanation-8.0' => 'O nome de usuario MySQL e o nome do banco de dados podem contar letras minusculas, numeros e "underline"(sublinhado) e deve ter entre 4 e 16 caracteres. Você não pode utilizar <a href="https://dev.mysql.com/doc/refman/8.0/en/reserved-words.html">palavras reservadas(link em inglês)</a>!',
 
     'maintenance-notice' => 'Cadastro não permitido devido a manutenção do banco de dados.',
 
@@ -41,7 +41,7 @@ $translations = array(
 
     'agm-li2' => 'db4free.net não é ideal para produtos finais',
 
-    'agm-li3' => 'Se você tentar utilizar o seu banco de dados db4free.net para produção final mesmo com os avisos, você faz isso por sua conta e risco (Backups frequentes são altamente recomendados)',
+    'agm-li3' => 'Se você tentar utilizar o seu banco de dados db4free.net para produção final mesmo com os avisos, você faz isso por sua conta e risco (recomendamos que realize backups frequentemente)',
 
     'agm-li4' => 'Perda de dados e queda de energia podem acontecer a qualquer momento (qualquer reclamação sobre esses assuntos sera ignorada)',
 
@@ -57,7 +57,7 @@ $translations = array(
 
     'agm-li9' => 'db4free.net é um serviço para teste, não para hospedagem. Bancos de dados que ocuparem mais de 200mb de espaço serão limpos em intervalos irregulares sem prévio aviso',
 
-    'agm-li10' => 'Por favor, remova os dados que você não precisa oo <a href="/delete-account.php">delete a conta que você não usa mais</a>. Isso torna mais fácil a recuperação do servidor em caso ele fique fora do ar.',
+    'agm-li10' => 'Por favor, remova os dados que você não precisa, ou <a href="/delete-account.php"> delete a conta que você não usa mais</a>. Isso torna mais fácil a recuperação do servidor em caso ele fique fora do ar.',
 
     'signup-error1' => 'Você tem que aceitar os termos de uso!',
 
